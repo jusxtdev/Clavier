bec52fa38abd7d7b573948014a9cfdf6f4b1b7ba
Dev Prajapati <pdev25214@gmail.com>
2026-05-31 17:07:31 +0000

    Allow Unauthenticated users to GET /products and GET /products/:id

--- backend/src/routes/product.router.ts
+++ backend/src/routes/product.router.ts
@@ -8,19 +8,18 @@ import express from "express";
 const router = express.Router();
 
 // Each route is accessible to logged in users only
-router.use(authenticate)
 
-router.get("/",  ProductController.getProducts);
+router.get("/", ProductController.getProducts);
 
 router.get("/:id", ProductController.getProductById);
 
 // only ADMIN can CREATE new products
-router.post("/", authorize(["ADMIN"]), validate(ProductSchema.newProduct), ProductController.createProduct)
+router.post("/", authenticate, authorize(["ADMIN"]), validate(ProductSchema.newProduct), ProductController.createProduct)
 
 // only ADMIN and STAFF can UPDATE a product (stock / price etc.)
-router.patch("/:id", authorize(["ADMIN", "STAFF"]), validate(ProductSchema.updateProduct), ProductController.updateProduct)
+router.patch("/:id", authenticate, authorize(["ADMIN", "STAFF"]), validate(ProductSchema.updateProduct), ProductController.updateProduct)
 
 // only ADMIN can DELETE products
-router.delete("/:id", authorize(["ADMIN"]), ProductController.deleteProduct)
+router.delete("/:id", authenticate, authorize(["ADMIN"]), ProductController.deleteProduct)
 
 export default router;
