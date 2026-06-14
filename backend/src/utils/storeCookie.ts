fed73c31b10912f487d621ef452ebf21b7582566
Dev Prajapati <pdev25214@gmail.com>
2026-06-14 15:08:43 +0000

    change sameSite from strict to lax

--- backend/src/utils/storeCookie.ts
+++ backend/src/utils/storeCookie.ts
@@ -3,8 +3,8 @@ import { Response } from "express";
 
 /**
  * Store a cookie in the response with the given title and data,
- * The cookie is configured to be HTTP-only, secure in production, 
- * with a max age of 7 days, and same-site policy set to "strict".
+ * The cookie is configured to be HTTP-only, secure in production,
+ * with a max age of 7 days, and same-site policy set to "lax" for cross-origin support.
  * @param title Title of the cookie to be stored ("jwt" for authentication tokens).
  * @param data The value to be stored in the cookie (e.g., JWT token).
  * @param res The Express Response object used to set the cookie.
@@ -14,7 +14,7 @@ const storeCookie = (title: string, data: any, res: Response) => {
     httpOnly: true,
     secure: env.NODE_ENV === "production",
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    sameSite: "strict",
+    sameSite: "lax",
   });
 };
 
